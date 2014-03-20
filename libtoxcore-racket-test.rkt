60ef174bb4f00c85ed16bbfbc2f52ac140263dc6
Lehi Toskin <lehi_toskin@lavabit.com>
2014-03-19 20:34:56 -0700

    Silly program for a start

--- libtoxcore-racket-test.rkt
+++ libtoxcore-racket-test.rkt
@@ -3,4 +3,8 @@
 (require racket/include)
 (include "libtoxcore-racket.rkt")
 
-(tox_get_client_id _Tox-pointer "someaddress")
+; initialize a new Tox and grab the _Tox-pointer
+(define my-tox (tox_new 0))
+
+; THIS KILLS THE TOX
+(tox_kill my-tox)
